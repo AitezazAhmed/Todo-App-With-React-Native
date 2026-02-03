4813c43a8975c19786617e3c35d5040e31448b8a
AitezazAhmed <stoneghold4103@gmail.com>
2026-02-03 15:01:38 +0500

    .

--- app/(tabs)/setting.tsx
+++ app/(tabs)/setting.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 const setting = () => {
   return (
-    <View>
-      <Text>setting</Text>
+    <View
+    style={styles.container} 
+    >
+      <Text
+      style={styles.text}
+      >setting</Text>
     </View>
   )
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,                // take full screen
+    justifyContent: 'center', // vertical center
+    alignItems: 'center',     // horizontal center
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+})
 export default setting
