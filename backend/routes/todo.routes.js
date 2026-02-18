4427505f1f4ba98ecc5550850a85ebb9ef31aaf2
AitezazAhmed <stoneghold4103@gmail.com>
2026-02-19 00:24:06 +0500

    .

--- backend/routes/todo.routes.js
+++ backend/routes/todo.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createTodo, getTodos } from "../controllers/todo.controller.js";
+import { createTodo, getTodos, updateTodo,deleteTodo,toggleTodo } from "../controllers/todo.controller.js";
 
 const router = express.Router();
 router.post("/", createTodo);
 router.get("/", getTodos);
-
+router.put("/:id", updateTodo);
+router.delete("/:id", deleteTodo);
+router.patch("/:id/toggle", toggleTodo);
 export default router;
