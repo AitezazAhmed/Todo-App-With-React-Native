15a45cce2da129719006735413941e5be42bc060
AitezazAhmed <stoneghold4103@gmail.com>
2026-03-11 05:34:34 +0500

    .

--- mobile/lib/api.js
+++ mobile/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://192.168.100.106:5000/api",
+  baseURL: "https://todo-app-with-react-native.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
